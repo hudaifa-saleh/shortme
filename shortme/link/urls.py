9f73db8f89ed087281035e431c05468818b9035e
hudaifa <hudaifa890@gmail.com>
2023-10-22 13:19:50 +0300

    add urls to the views

--- shortme/link/urls.py
+++ shortme/link/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
-from shortme.link.views import ShortMeView
+from shortme.link.views import HomeView, ShortMeView
 
 app_name = 'link'
 urlpatterns = [
+    re_path(r'^$', HomeView.as_view(), name='home'),
     re_path(r'link/(?P<shortcode>[\w-]){6,15}$', ShortMeView.as_view(), name='get-link'),
 ]
