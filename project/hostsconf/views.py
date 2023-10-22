421649c7efb9972b320a47d774363a959c0f566c
hudaifa <hudaifa890@gmail.com>
2023-10-22 19:04:50 +0300

    add url

--- project/hostsconf/views.py
+++ project/hostsconf/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
-DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL", "https://www.kirr.co")
+DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL", "https://www.shortme.com")
 
 
 
